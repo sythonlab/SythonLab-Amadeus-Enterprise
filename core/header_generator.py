83e70618c422a1eb11a63904dae0bf2927d3c0ef
Jose Angel <jaalvarez2818@gmail.com>
2025-08-04 15:30:12 +0200

    Fix Amadeus Headers

--- core/header_generator.py
+++ core/header_generator.py
@@ -1,6 +1,6 @@
 import base64
 import hashlib
-import uuid
+import os
 from datetime import datetime, timezone
 
 
@@ -19,46 +19,33 @@ class AmadeusHeaderGenerator:
         pass
 
     def generate_message_id(self):
-        code = f'{str(uuid.uuid4())}{str(uuid.uuid4())}'.replace('-', '')
-        self.message_id = f'WbsConsu-{code[:31]}-{code[31:40]}'
-
-        print('MESSAGE ID', self.message_id)
+        random_bytes = os.urandom(24)
+        b64_encoded = base64.urlsafe_b64encode(random_bytes).decode('ascii')
+        clean_b64 = b64_encoded.replace('=', '').replace('_', '-').replace('/', '')
+        self.message_id = clean_b64[:43]
 
     def generate_action(self, target):
         self.action = {
             'availability': 'http://webservices.amadeus.com/FMPTBQ_24_1_1A'
         }[target]
 
-        print('ACTION', self.action)
-
     def generate_to(self, target):
         self.to = {
             'availability': 'https://nodeD1.test.webservices.amadeus.com/1ASIWCARCPJ'
         }[target]
 
-        print('TO', self.to)
-
     def generate_nonce(self):
-        # self.nonce_bytes = os.urandom(16)
-        self.nonce_bytes = str(uuid.uuid4()).replace('-', '')[:16].encode('utf-8')
+        self.nonce_bytes = os.urandom(16)
         self.nonce = base64.b64encode(self.nonce_bytes).decode('utf-8')
 
-        print('NONCE BYTES', self.nonce_bytes)
-        print('NONCE', self.nonce)
-
     def generate_created_at(self):
-        self.created_at = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-
-        print('CREATED', self.created_at)
+        self.created_at = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
 
     def generate_password_digest(self):
         password_bytes = '6WbmmXZJGS?D'.encode('utf-8')
-        concatenated = self.nonce_bytes + self.created_at.encode('utf-8') + password_bytes
+        concatenated = self.nonce_bytes + self.created_at.encode('utf-8') + hashlib.sha1(password_bytes).digest()
         digest = hashlib.sha1(concatenated).digest()
         self.password_digest = base64.b64encode(digest).decode('utf-8')
-
-        print('PASSWORD DIGEST', self.password_digest, 'GENERATED WITH PASSWORD BYTES', password_bytes, 'CREATED',
-              self.created_at, 'AND NONCE BYTES', self.nonce_bytes)
 
     def generate_header(self, target):
         self.generate_message_id()
--- test.py
+++ test.py
@@ -1,83 +1,27 @@
-from zeep import Client
-from zeep.exceptions import Fault
-from zeep.plugins import HistoryPlugin
-from lxml import etree
+import requests
+from core.header_generator import AmadeusHeaderGenerator
+from flights.queries import AVAILABILITY_QUERY
 
-import settings
-from core.enterprise_core import AmadeusEnterpriseSDK
+headers = AmadeusHeaderGenerator()
+headers.generate_header('availability')
 
-sdk = AmadeusEnterpriseSDK()
-
-headers_data = sdk.get_headers('availability', 1)
-message_id_header = headers_data['message_id']['header']
-action_header = headers_data['action']['header']
-to_header = headers_data['to']['header']
-session_header = headers_data['session']['header']
-security_header = headers_data['security']['header']
-user_id_header = headers_data['user_id']['header']
-
-history = HistoryPlugin()
-
-client = Client(
-    wsdl=settings.AMADEUS_CONFIG['WSDL'],
-    plugins=[history]
-)
-
-body = {
-    'numberOfUnit': {
-        'unitNumberDetail': [
-            {'numberOfUnits': 2, 'typeOfUnit': 'PX'},
-            {'numberOfUnits': 100, 'typeOfUnit': 'RC'}
-        ]
-    },
-    'paxReference': {
-        'ptc': 'ADT',
-        'traveller': [{'ref': 1}, {'ref': 2}]
-    },
-    'fareOptions': {
-        'pricingTickInfo': {
-            'pricingTicketing': {
-                'priceType': ['ET', 'RP', 'RU', 'TAC', 'XND', 'XLA', 'XLO', 'XLC', 'XND']
-            }
-        }
-    },
-    'travelFlightInfo': {
-        'companyIdentity': {
-            'carrierQualifier': 'X',
-            'carrierId': ['NK', 'F9']
-        },
-        'flightDetail': {
-            'flightType': 'N'
-        }
-    },
-    'itinerary': [
-        {
-            'requestedSegmentRef': {'segRef': 1},
-            'departureLocalization': {'departurePoint': {'locationId': 'HAV'}},
-            'arrivalLocalization': {'arrivalPointDetails': {'locationId': 'MAD'}},
-            'timeDetails': {'firstDateTimeDetail': {'date': '2025-08-06'}}
-        },
-        {
-            'requestedSegmentRef': {'segRef': 2},
-            'departureLocalization': {'departurePoint': {'locationId': 'MAD'}},
-            'arrivalLocalization': {'arrivalPointDetails': {'locationId': 'HAV'}},
-            'timeDetails': {'firstDateTimeDetail': {'date': '2025-08-13'}}
-        }
-    ]
+http_headers = {
+    "Content-Type": "application/soap+xml; charset=utf-8",
+    "SOAPAction": "http://webservices.amadeus.com/FMPTBQ_23_1_1A"
 }
 
-try:
-    response = client.service.Fare_MasterPricerTravelBoardSearch(
-        **body,
-        _soapheaders=[message_id_header, action_header, to_header, session_header, security_header, user_id_header]
-    )
-except Fault as e:
-    print(f"SOAP Fault: {e}")
+response = requests.post(
+    "https://nodeD1.test.webservices.amadeus.com/1ASIWCARCPJ",
+    data=AVAILABILITY_QUERY.format(
+        MESSAGE_ID=headers.message_id,
+        USERNAME=headers.username,
+        NONCE=headers.nonce,
+        PASSWORD_DIGEST=headers.password_digest,
+        CREATED_AT=headers.created_at
+    ).encode("utf-8"),
+    headers=http_headers,
+    timeout=10
+)
 
-if history.last_sent:
-    print("Request XML:")
-    print(etree.tostring(history.last_sent['envelope'], pretty_print=True).decode())
-
-if history.last_received:
-    print("Response XML:")
-    print(etree.tostring(history.last_received['envelope'], pretty_print=True).decode())
+print(response.status_code)
+print(response.text)
